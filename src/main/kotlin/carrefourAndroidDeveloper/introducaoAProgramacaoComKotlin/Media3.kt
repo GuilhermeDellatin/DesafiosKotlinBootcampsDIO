fdbaecdd10128e88b09f5a29f548a132722eaf8e
GuilhermeDellatin <guidellatin@gmail.com>
2021-08-24 22:22:34 -0300

    Introdução a Programação em Kotlin, Carrefour, exercícios idade em dias e media 3

--- src/main/kotlin/carrefourAndroidDeveloper/introducaoAProgramacaoComKotlin/Media3.kt
+++ src/main/kotlin/carrefourAndroidDeveloper/introducaoAProgramacaoComKotlin/Media3.kt
@@ -37,3 +37,37 @@ Exemplo de Entrada:	Exemplo de Saída:
 9.0 4.0 8.5 9.0     Media: 7.3
                     Aluno aprovado.
  */
+
+import java.util.*
+
+fun main() {
+    val entrada = Scanner(System.`in`)
+    val n1 = entrada.nextFloat()
+    val n2 = entrada.nextFloat()
+    val n3 = entrada.nextFloat()
+    val n4 = entrada.nextFloat()
+
+    var media = ((n1*2) + (n2*3) + (n3*4) + (n4*1)) / 10.0
+    println("Media: ${"%.1f".format(media)}")
+
+    if(media >= 7) {
+        println("Aluno aprovado.")
+    }
+    else {
+        if (media < 5) {
+            println("Aluno reprovado.")
+        }
+        if (media >= 5.0 && media <= 6.9) {
+            println("Aluno em exame.")
+            val exame = entrada.nextFloat()
+            println("Nota do exame: ${"%.1f".format(exame)}")
+            media = (media + exame) / 2
+            if (media >= 5) {
+                println("Aluno aprovado.")
+            } else {
+                println("Aluno reprovado")
+            }
+            println("Media final: ${"%.1f".format(media)}")
+        }
+    }
+}
