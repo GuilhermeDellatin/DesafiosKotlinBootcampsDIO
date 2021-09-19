c29e123a74e9c52cbb4b3d205cd4c80e51eca922
GuilhermeDellatin <guidellatin@gmail.com>
2021-09-19 14:20:52 -0300

    Desafios Numericos em Kotlin, Ajuste TDA Racional

--- src/main/kotlin/carrefourAndroidDeveloper/desafiosNumericosEmKotlin/TDAracional.kt
+++ src/main/kotlin/carrefourAndroidDeveloper/desafiosNumericosEmKotlin/TDAracional.kt
@@ -37,55 +37,76 @@ Exemplo de Entrada:	Exemplo de Saída:
 
  */
 
-import java.util.*
+import java.util.Scanner
 
-fun main() {
-    val input = Scanner(System.`in`)
-    val n = input.nextInt()
-    for (j in 0 until n) {
-        val N1 = input.nextInt()
-        input.next()
-        val D1 = input.nextInt()
-        val operacao = input.next()[0]
-        val N2 = input.nextInt()
-        input.next()
-        val D2 = input.nextInt()
-        var N3 = 0
-        var D3 = 1
-        when (operacao) {
-            '+' -> {
-                N3 = N1 * D2 + N2 * D1
-                D3 = D1 * D2
+fun main(args: Array<String>) {
+
+    var scan = Scanner(System.`in`)
+    val n: Int
+    var n1: Int
+    var d1: Int
+    var n2: Int
+    var d2: Int
+    var simp: Int = 0
+    var nf: Int = 0
+    var df: Int = 0
+    var ns: Int = 0
+    var ds: Int = 0
+    var aux: String
+
+    n = scan.nextInt()
+
+    for (i in 1..n) {
+        n1 = scan.nextInt()
+        scan.next()
+        d1 = scan.nextInt()
+        aux = scan.next()
+        n2 = scan.nextInt()
+        scan.next()
+        d2 = scan.nextInt()
+
+        when (aux) {
+            "+" -> {
+                ns = (n1 * d2) + (n2 * d1)
+                ds = (d1 * d2)
             }
-            '-' -> {
-                N3 = N1 * D2 - N2 * D1
-                D3 = D1 * D2
+            "-" -> {
+                ns = (n1 * d2) - (n2 * d1)
+                ds = (d1 * d2)
             }
-            '/' -> {
-                N3 = N1 * D2
-                D3 = N2 * D1
+            "*" -> {
+                ns = (n1 * n2)
+                ds = (d1 * d2)
             }
-            '*' -> {
-                N3 = N1 * N2
-                D3 = D1 * D2
+            "/" -> {
+                ns = (n1 * d2)
+                ds = (n2 * d1)
             }
         }
-        val d3 = D3
-        val n3 = N3
-        val max = Math.max(N3, D3)
-        var irredutivel = false
-        while (!irredutivel) {
-            irredutivel = true
-            for (i in max - 1 downTo 2) {
-                if (N3 % i == 0 && D3 % i == 0) {
-                    N3 = N3 / i
-                    D3 = D3 / i
-                    irredutivel = false
-                }
+
+        fun mdc(a: Int, b: Int): Int {
+            var r: Int
+            var aa: Int = a
+            var bb: Int = b
+
+            while (bb != 0) {
+                r = aa % bb
+                aa = bb
+                bb = r
             }
+
+            if (aa < 0) {
+                aa = aa * (-1)
+            }
+            return aa
         }
-        println("$n3/$d3 = $N3/$D3")
+
+        nf = ns / mdc(ds, ns)
+        df = ds / mdc(ds, ns)
+
+        println("$ns/$ds = $nf/$df")
     }
+
 }
 
 
